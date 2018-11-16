680ea0489bea56573c223f7dc8fc57fe3d4bc7d7
Jeff <jeffrey.lord@sap.com>
2018-11-16 13:30:10 -0500

    Added a lot of comments

--- pdfSplit.py
+++ pdfSplit.py
@@ -10,46 +10,65 @@ def parse_args(args):
     parser = argparse.ArgumentParser(description='arguments')
     parser.add_argument('--pdf', '-p', help='path to pdf')
     parser.add_argument(
-        '--dest', '-d', help='destination for split pages', default='/')
+        '--dest', '-d', help='destination for split pages', default='split')
     return parser.parse_args(args)
 
 
 def main(args):
+    # Parse the arguments passed from command line
     args = parse_args(args)
-    pdf_file = open(args.pdf, 'rb')
+    # Call split function
+    split_pdf(args.pdf, args.dest)
+
+
+def split_pdf(pdf_path, destination):
+    """Function to split a single pdf into single page pdfs."""
+    # Open the pdf specified by the pdf argument
+    pdf_file = open(pdf_path, 'rb')
+    # Open pdf with PdfFileReader
     pdf_reader = PdfFileReader(pdf_file)
+    # Get number of pages
     page_numbers = pdf_reader.getNumPages()
-    base_name = os.path.basename(args.pdf)
-    base_name_ne = os.path.splitext(base_name)[0]
-
-    # start_time = time.time()
-    # connection = connect_hdb()
-    # end_time = time.time()
-    # print('[INFO] HDB Connection Time: ' + str(end_time - start_time))
-    # cursor = connection.cursor()
-
-    if not os.path.exists(args.dest):
-        os.makedirs(args.dest)
+    # This gets the "pdf_name.pdf" from the path given
+    file_folder = os.path.basename(os.path.normpath(pdf_path))
+    # This removes the .pdf so it just has the name of the pdf (not really necessary)
+    file_folder = os.path.splitext(file_folder)[0]
+    # Make path for split pages (the pages will be in a folder named whatever the pdf name is)
+    path = os.path.join(destination, file_folder)
+    print('[INFO] Split page output path: ' + path)
+    # If the destination for split pages does not exist, create it (else an error will occur)
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print("Creating path {0}".format(path))
+    # Start timer
     start_time = time.time()
     print('[INFO] Processing pdfs...')
+    print('[INFO] Pages found: {0}'.format(page_numbers))
+    # Loop the number of pages in pdf
     for i in range(page_numbers):
+        # Create pdf writer object
         pdf_writer = PdfFileWriter()
+        # Add single pdf page based on loop index
         pdf_writer.addPage(pdf_reader.getPage(i))
+        # Make path for storing split page
         new_page_path = os.path.join(
-            args.dest, base_name_ne + '_page' + str(i + 1) + '.pdf')
+            path, 'page_' + str(i + 1) + '.pdf')
+        print('[INFO] Processing: {0}'.format(new_page_path))
+        # Open new file at path of new split page
         new_page = open(new_page_path, 'wb')
+        # Write file to the split page path
         pdf_writer.write(new_page)
+    # Stop timer
     end_time = time.time()
-    print('[INFO] PDF Processing Time: ' + str(end_time - start_time))
-    print('[INFO] Time per page: ' +
-          str((end_time - start_time) / page_numbers))
-    print('[INFO] Pages per second: ' +
-          str(page_numbers / (end_time - start_time)))
-
-    # connection.close()
+    print('[INFO] PDF Processing Time: {0}'.format(str(end_time - start_time)))
+    print('[INFO] Time per page: {0}'.format(
+          str((end_time - start_time) / page_numbers)))
+    print('[INFO] Pages per second: {0}'.format(
+          str(page_numbers / (end_time - start_time))))
 
 
 def connect_hdb():
+    """Connect to Hana Database and return the connection object."""
     connection = pyhdb.connect(
         host="host",
         port=12345,
